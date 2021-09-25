26b2f01c2dc1a53f34790807291bd4bbaec8bace
glen-s-abraham <glenprojects666@gmail.com>
2021-09-25 12:01:59 +0530

    refactor:added validators to ToDo model

--- model/ToDo.js
+++ model/ToDo.js
@@ -5,11 +5,13 @@ const toDoSchema = new mongoose.Schema({
     task: {
         type:String,
         required:[true, 'A task must be mentioned'],
-        trim:true
+        trim:true,
+        max:[100,'Task length must be within 100 words']
     },
     reminder_date:{
         type:Date,
         required:[true, 'A task must have a reminder date'],
+        min:[Date.now(),'A taks can\'t be assigned to the past']
     },
     createdAt:{
         type:Date,
