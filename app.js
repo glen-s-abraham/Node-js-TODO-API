1c994923416b1e6a82a637c3d9eb179ef7df0111
glen-s-abraham <glenprojects666@gmail.com>
2021-09-27 12:23:10 +0530

    refactor:erro handler for unhandeld routes

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const morgan = require('morgan');
 const express = require('express');
-const ToDoRouter = require('./routes/ToDoRoutes');
-
 const app = express();
+const APIError = require('./utils/AppError');
+const ToDoRouter = require('./routes/ToDoRoutes');
+const AppError = require('./utils/AppError');
+
+
 app.use(express.json());
 app.use(morgan('dev'));
 
-
 app.use('/api/to-do',ToDoRouter);
+//unhandeld route exception
+app.all('*',(req,res,next)=>{
+    next(new AppError(`Can't find ${req.originalUrl}`,404));
+});
+
 
 module.exports = app;
