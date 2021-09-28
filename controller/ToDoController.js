9778c1c43c8e8d3582a10a2cbab7695a603d22a0
glen-s-abraham <glenprojects666@gmail.com>
2021-09-28 12:49:10 +0530

    refactor:added exception handler for async functions fix:null return of single to do response fixed with error response

--- controller/ToDoController.js
+++ controller/ToDoController.js
@@ -1,92 +1,56 @@
+const AppError = require('../utils/AppError');
 const ToDo = require('./../model/ToDo');
 const APIFeatures = require('./../utils/APIFeatures');
+const catchAsync = require('./../utils/CatchAsync');
 
-exports.createToDo = async (req,res)=>{
-    try{ 
-        const todo = await ToDo.create(req.body);
-        res.status(200).json({
-            status: 'success',
-            todo
-        });
-    }
-    catch(err){
-        res.status(400).json({
-            status: 'fail',
-            err
-        });
-    }
+exports.createToDo = catchAsync(async (req,res,next)=>{
+    const todo = await ToDo.create(req.body);
+    res.status(200).json({
+        status: 'success',
+        todo
+    });
+});
     
-}
 
-exports.updateToDo = async (req,res)=>{
-    try{
-        const toDo = await ToDo.findByIdAndUpdate(req.params.id,req.body,{
-            new:true,
-            runValidators:true
-        });
-        res.status(200).json({
-            status: 'success',
-            toDo
-        });
-    }
-    catch(err){
-        res.status(400).json({
-            status: 'fail',
-            err
-        });
-    }
-}
-
-exports.deleteToDo = async (req,res)=>{
-    try{
-        await ToDo.findByIdAndDelete(req.params.id);
-        res.status(204).json({
-            status: 'success',
-            message:null
-        });
-    }
-    catch(err){
-        res.status(400).json({
-            status: 'fail',
-            err
-        });
-    }
-}
-
-exports.getAllToDos = async (req,res)=>{
-    try{
-
-        const features = new APIFeatures(ToDo.find(),req.query)
-                        .filter().sort().limit().paginate();;
-        
-        const toDos = await features.query;
-        res.status(200).json({
-            status: 'success',
-            toDos
-        });
-    }
-    catch(err){
-        res.status(400).json({
-            status: 'fail',
-            err
-        });
-    }
+exports.updateToDo = catchAsync(async (req,res,next)=>{
+    const toDo = await ToDo.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true
+    });
+    res.status(200).json({
+        status: 'success',
+        toDo
+    });
+});
     
-}
 
-exports.getSingleToDo = async (req,res)=>{
-    try{
-        const toDo = await ToDo.findById(req.params.id);
-        res.status(200).json({
-            status: 'success',
-            toDo
-        });
-    }
-    catch(err){
-        res.status(400).json({
-            status: 'fail',
-            err
-        });
-    }
-}
+exports.deleteToDo = catchAsync(async (req,res,next)=>{
+    await ToDo.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+        status: 'success',
+        message:null
+    });
+});
+    
 
+exports.getAllToDos = catchAsync(async (req,res,next)=>{
+    const features = new APIFeatures(ToDo.find(),req.query)
+        .filter().sort().limit().paginate();;
+       
+    const toDos = await features.query;
+    res.status(200).json({
+        status: 'success',
+        toDos
+    });
+});
+    
+exports.getSingleToDo = catchAsync(async (req,res,next)=>{
+    const toDo = await ToDo.findById(req.params.id);
+    if(!toDo){
+        return next(new AppError('Object not found',404));
+    }
+    res.status(200).json({
+        status: 'success',
+        toDo
+    });
+});
